a98b5f2a97d4d113ab4e2ac7964112d99d460360
rallerG <raller799@live.dk>
2018-07-29 15:23:49 +0200

    Added frames and a ball

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var leftPlayer;
 var rightPlayer;
+var ball;
 
 window.onload = function() {
     gameArea.start();
     leftPlayer = new component(20, 150, "white", 30, gameArea.canvas.height / 2 - 75);
     rightPlayer = new component(20, 150, "white", gameArea.canvas.width - 50, gameArea.canvas.height / 2 - 75);
+    ball = new component(25, 25, "white", gameArea.canvas.width / 2, gameArea.canvas.height / 2);
 }
 
 
@@ -16,18 +18,24 @@ var gameArea = {
         this.canvas.height = (window.innerHeight - 20);
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        placeOutline();
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
+
 
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y; 
-    ctx = gameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.update = function() {
+        ctx = gameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 
 function placeOutline() {
@@ -43,6 +51,14 @@ function placeOutline() {
     ctx.strokeStyle = "#FFFFFF";
     ctx.lineWidth = 12;
     ctx.stroke();
+}
+
+function updateGameArea() {
+    gameArea.clear();
+    placeOutline();
+    leftPlayer.update();
+    rightPlayer.update();
+    ball.update();
 }
 
 function setScore() {
